f050bcaf0f24cf5d7a01a627b621c9751d390020
Sohail Shiraj <sohail.shiraj@hotmail.com>
2021-11-21 23:53:30 -0500

    contact us page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Sohail Shiraj Portfolio' });
+});
+
+router.get('/about', (req, res, next) => {
+  res.render('about', { title: 'About Me'})
+});
+
+router.get('/projects', (req, res, next) => {
+  res.render('projects', { title: 'Projects'})
+});
+
+router.get('/contact', (req, res, next) => {
+  res.render('contact', { title: 'Contact Me'})
 });
 
 module.exports = router;
