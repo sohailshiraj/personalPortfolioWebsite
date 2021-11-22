c20cb7935330cc1875f79f718c92255876b6d813
Sohail Shiraj <sohail.shiraj@hotmail.com>
2021-11-22 14:22:00 -0500

    added Social link  to footer, adjustments to styles, added comments

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Sohail Shiraj Portfolio' });
+  res.render('index', { title: 'Sohail Shiraj' });
 });
 
 router.get('/about', (req, res, next) => {
